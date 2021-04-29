dec6f510cb81dcc3f7e1c6cad2c9d1eb00f101b8
Brion Vibber <brion@pobox.com>
2021-04-29 10:59:33 -0700

    work in progress

--- src/main.rs
+++ src/main.rs
@@ -21,18 +21,24 @@ use thiserror::Error;
 
 type Result<T> = std::result::Result<T, LocalError>;
 
-enum Depth {
-    RGB24,
-    RGBA32Float,
+struct PixelReader {
+    bytes_per_pixel: usize,
+    read_rgb: fn(data: &[u8]) -> Vec3,
 }
 
-impl Depth {
-    fn bytes_per_pixel(&self) -> usize {
-        match self {
-            Depth::RGB24 => 3,
-            Depth::RGBA32Float => 16,
-        }
-    }
+fn read_rgb24(data: &[u8]) -> Vec3 {
+    let scale = Vec3::splat(1.0 / 255.0);
+    Vec3::new(data[0] as f32, data[1] as f32, data[2] as f32) * scale
+}
+
+fn read_rgb128float(data: &[u8]) -> Vec3 {
+    let data_ref_f32: &f32 = unsafe {
+        std::mem::transmute(&data[0])
+    };
+    let data_f32 = unsafe {
+        std::slice::from_raw_parts(data_ref_f32, data.len())
+    };
+    Vec3::new(data_f32[0], data_f32[1], data_f32[2])
 }
 
 struct Options {
@@ -75,10 +81,15 @@ fn time_func<F, G>(msg: &str, func: F) -> Result<G>
 // Read an input PNG and return its size and contents
 // It must be a certain format (8bpp true color no alpha)
 fn read_png(filename: &str)
-    -> Result<(u32, u32, Depth, Vec<u8>)>
+    -> Result<(u32, u32, PixelReader, Vec<u8>)>
 {
     use png::Decoder;
     use png::Transformations;
+
+    let pixel = PixelReader {
+        bytes_per_pixel: 3,
+        read_rgb: read_rgb24,
+    };
 
     let mut decoder = Decoder::new(File::open(filename)?);
     decoder.set_transformations(Transformations::IDENTITY);
@@ -95,17 +106,20 @@ fn read_png(filename: &str)
     let mut data = vec![0u8; info.buffer_size()];
     reader.next_frame(&mut data)?;
 
-    Ok((info.width, info.height, Depth::RGB24, data))
+    Ok((info.width, info.height, pixel, data))
 }
 
 fn read_jxr(filename: &str)
-  -> Result<(u32, u32, Depth, Vec<u8>)>
+  -> Result<(u32, u32, PixelReader, Vec<u8>)>
 {
     use jpegxr::ImageDecode;
     use jpegxr::PixelFormat;
     use jpegxr::Rect;
 
-    let depth = Depth::RGBA32Float;
+    let pixel = PixelReader {
+        bytes_per_pixel: 16,
+        read_rgb: read_rgb128float,
+    };
 
     let input = File::open(filename)?;
     println!("creating");
@@ -118,7 +132,7 @@ fn read_jxr(filename: &str)
     }
 
     let (width, height) = decoder.get_size()?;
-    let stride = width as usize * depth.bytes_per_pixel();
+    let stride = width as usize * pixel.bytes_per_pixel;
     let size = stride * height as usize;
     let mut data = Vec::<u8>::with_capacity(size);
     data.resize(size, 0);
@@ -128,7 +142,7 @@ fn read_jxr(filename: &str)
     let rect = Rect::new(0, 0, width, height);
     decoder.copy(&rect, &mut data, stride as u32)?;
 
-    Ok((width as u32, height as u32, depth, data))
+    Ok((width as u32, height as u32, pixel, data))
 }
 
 fn pq_to_linear(val: Vec3) -> Vec3 {
@@ -265,17 +279,19 @@ fn hdr_to_sdr_pixel(rgb_bt2100: Vec3, options: &Options) -> Vec3
 const SCALE_OUT_8: f32 = 255.0;
 const SCALE_IN_8: f32 = 1.0 / SCALE_OUT_8;
 
-fn hdr_to_sdr(data: &mut [u8], options: &Options)
+fn hdr_to_sdr(pixel: &PixelReader, in_data: &[u8], out_data: &mut [u8], options: &Options)
 {
-    let scale_in = Vec3::splat(SCALE_IN_8);
     let scale_out = Vec3::splat(SCALE_OUT_8);
-    data.par_chunks_mut(3).for_each(|rgb| {
-        let rgb_bt2100 = Vec3::new(rgb[0] as f32, rgb[1] as f32, rgb[2] as f32) * scale_in;
+    let in_iter = in_data.par_chunks(pixel.bytes_per_pixel);
+    let out_iter = out_data.par_chunks_mut(3);
+    let iter = in_iter.zip(out_iter);
+    iter.for_each(|(rgb_in, rgb_out)| {
+        let rgb_bt2100 = (pixel.read_rgb)(rgb_in);
         let rgb_srgb = hdr_to_sdr_pixel(rgb_bt2100, options);
         let rgb_8 = rgb_srgb * scale_out;
-        rgb[0] = rgb_8.x as u8;
-        rgb[1] = rgb_8.y as u8;
-        rgb[2] = rgb_8.z as u8;
+        rgb_out[0] = rgb_8.x as u8;
+        rgb_out[1] = rgb_8.y as u8;
+        rgb_out[2] = rgb_8.z as u8;
     });
 }
 
@@ -309,7 +325,7 @@ fn write_png(filename: &str,
 
 fn hdrfix(args: ArgMatches) -> Result<String> {
     let input_filename = args.value_of("input").unwrap();
-    let (width, height, depth, mut data) = time_func("read_input", || {
+    let (width, height, pixel, mut in_data) = time_func("read_input", || {
         let ext = Path::new(&input_filename).extension().unwrap().to_str().unwrap();
         match ext {
             "png" => read_png(input_filename),
@@ -335,13 +351,16 @@ fn hdrfix(args: ArgMatches) -> Result<String> {
             _ => unreachable!("bad color-map option")
         },
     };
+    let out_size = width as usize * height as usize * 3;
+    let mut out_data = Vec::<u8>::with_capacity(out_size);
+    out_data.resize(out_size, 0);
     time_func("hdr_to_sdr", || {
-        Ok(hdr_to_sdr(&mut data, &options))
+        Ok(hdr_to_sdr(&pixel, &in_data, &mut out_data, &options))
     })?;
 
     let output_filename = args.value_of("output").unwrap();
     time_func("write_png", || {
-        write_png(output_filename, width, height, &data)
+        write_png(output_filename, width, height, &out_data)
     })?;
 
     return Ok(output_filename.to_string());
