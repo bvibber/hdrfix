635562a52def326dd1a90c714a01b8dad21b304e
Brion Vibber <brion@pobox.com>
2021-04-30 22:58:49 -0700

    remove debug

--- src/jpegxr.rs
+++ src/jpegxr.rs
@@ -171,10 +171,6 @@ impl PixelFormat {
                 return Ok(*map_val);
             }
         }
-        println!("{0:#x} {1:#x} {2:#x} {3:#x} {4:#x} {5:#x} {6:#x} {7:#x} {8:#x} {9:#x} {10:#x}", guid.Data1, guid.Data2, guid.Data3,
-            guid.Data4[0], guid.Data4[1], guid.Data4[2], guid.Data4[3],
-            guid.Data4[4], guid.Data4[5], guid.Data4[6], guid.Data4[7]
-        );
         Err(UnrecognizedPixelFormat)
     }
 
