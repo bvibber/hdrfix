977e878780c51b5bf82636fecc3685cb08cf94e5
Brion Vibber <brion@pobox.com>
2021-05-01 16:16:00 -0700

    fixes for linux cleanup

--- test.sh
+++ test.sh
@@ -1,10 +1,12 @@
 convert() {
-    cargo run --release --sdr-white=200 --hdr-max=2000 --tone-map=reinhard-rgb samples/"$1".png samples/"$1"-sdr.png
+    cargo run --release -- --sdr-white=200 --hdr-max=1000 --tone-map=reinhard-rgb samples/"$1"-hdr.jxr samples/"$1"-sdr-float.png
+    cargo run --release -- --sdr-white=200 --hdr-max=1000 --tone-map=reinhard-rgb samples/"$1".png samples/"$1"-sdr.png
 }
 
 convert burbank
 convert closeup
 convert cloud
+convert ikea
 convert newyork
 convert oregon
 convert panel
