f8f9630d98f55c8fa086b9adf75827fd0bab334e
Brion Vibber <brion@pobox.com>
2021-04-30 21:12:18 -0700

    cleanup on pixel buffers

--- src/main.rs
+++ src/main.rs
@@ -21,31 +21,41 @@ use thiserror::Error;
 
 type Result<T> = std::result::Result<T, LocalError>;
 
+// Note: currently assumes stride == width
 struct PixelBuffer {
     width: usize,
     height: usize,
-    stride: usize,
     bytes_per_pixel: usize,
     data: Vec::<u8>,
+}
+
+// If you wanted these could be traits
+// but we don't need that level of complexity
+struct PixelSource {
+    buffer: PixelBuffer,
     read_rgb_func: fn(&[u8]) -> Vec3,
+}
+
+struct PixelSink {
+    buffer: PixelBuffer,
     write_rgb_func: fn(&mut [u8], Vec3),
 }
 
 impl PixelBuffer {
-    fn new_srgb_rgb8(width: usize, height: usize) -> Self {
-        let bytes_per_pixel = 3;
+    fn new(width: usize, height: usize, bytes_per_pixel: usize) -> Self {
         let stride = width * bytes_per_pixel;
         let size = stride * height;
         let mut data = Vec::<u8>::with_capacity(size);
         data.resize(size, 0);
+        Self::with_vec(width, height, bytes_per_pixel, data)
+    }
+
+    fn with_vec(width: usize, height: usize, bytes_per_pixel: usize, data: Vec<u8>) -> Self {
         PixelBuffer {
             width: width,
             height: height,
-            stride: stride,
             bytes_per_pixel: bytes_per_pixel,
             data: data,
-            read_rgb_func: read_srgb_rgb24,
-            write_rgb_func: write_srgb_rgb24,
         }
     }
 
@@ -72,30 +82,62 @@ impl PixelBuffer {
     fn par_iter_mut(&mut self) -> rayon::slice::ChunksMut<u8> {
         self.data.par_chunks_mut(self.bytes_per_pixel)
     }
+}
 
-    fn read_rgb(&self, in_data: &[u8]) -> Vec3 {
-        (self.read_rgb_func)(in_data)
+impl PixelSource {
+    fn bt2100_rgb24(buffer: PixelBuffer) -> Self {
+        if buffer.bytes_per_pixel != 3 {
+            panic!("wrong bit depth");
+        }
+        PixelSource {
+            buffer: buffer,
+            read_rgb_func: read_bt2100_rgb24,
+        }
     }
 
-    fn write_rgb(&self, out_data: &mut [u8], rgb: Vec3) {
-        (self.write_rgb_func)(out_data, rgb)
+    fn scrgb_rgb128float(buffer: PixelBuffer) -> Self {
+        if buffer.bytes_per_pixel != 16 {
+            panic!("wrong bit depth");
+        }
+        PixelSource {
+            buffer: buffer,
+            read_rgb_func: read_scrgb_rgb128float,
+        }
     }
 
-    fn map_into<F>(&self, dest: &mut PixelBuffer, func: F)
+    fn read_rgb(&self, bytes: &[u8]) -> Vec3 {
+        (self.read_rgb_func)(bytes)
+    }
+
+    fn map_into<F>(&self, dest: &mut PixelSink, func: F)
     where F: (Fn(Vec3) -> Vec3) + Sync + Send
     {
-        // hack: get this first
         let writer = dest.write_rgb_func;
 
-        let in_iter = self.par_iter();
-        let out_iter = dest.par_iter_mut();
+        let in_iter = self.buffer.par_iter();
+        let out_iter = dest.buffer.par_iter_mut();
         let iter = in_iter.zip(out_iter);
 
         iter.for_each(|(bytes_in, bytes_out)| {
             let input_rgb = self.read_rgb(bytes_in);
             let output_rgb = func(input_rgb);
+            //dest.write_rgb(bytes_out, output_rgb);
             writer(bytes_out, output_rgb);
         });
+    }
+}
+
+impl PixelSink {
+
+    fn srgb_rgb24(buffer: PixelBuffer) -> Self {
+        PixelSink {
+            buffer: buffer,
+            write_rgb_func: write_srgb_rgb24,
+        }
+    }
+
+    fn write_rgb(&self, bytes: &mut [u8], rgb: Vec3) {
+        (self.write_rgb_func)(bytes, rgb)
     }
 }
 
@@ -104,17 +146,6 @@ fn read_bt2100_rgb24(data: &[u8]) -> Vec3 {
     let rgb_bt2100 = Vec3::new(data[0] as f32, data[1] as f32, data[2] as f32) * scale;
     let rgb_linear = pq_to_linear(rgb_bt2100);
     bt2100_to_scrgb(rgb_linear)
-}
-
-fn read_srgb_rgb24(data: &[u8]) -> Vec3 {
-    let scale = Vec3::splat(1.0 / 255.0);
-    let rgb_srgb = Vec3::new(data[0] as f32, data[1] as f32, data[2] as f32) * scale;
-    srgb_to_linear(rgb_srgb)
-}
-
-fn srgb_to_linear(_val: Vec3) -> Vec3 {
-    // may change these interfaces so can't happen
-    panic!("not implemented, currently not used");
 }
 
 fn write_srgb_rgb24(data: &mut [u8], val: Vec3)
@@ -135,18 +166,6 @@ fn read_scrgb_rgb128float(data: &[u8]) -> Vec3 {
         std::slice::from_raw_parts(data_ref_f32, data.len())
     };
     Vec3::new(data_f32[0], data_f32[1], data_f32[2])
-}
-
-fn write_scrgb_rgb128float(data: &mut [u8], val: Vec3) {
-    let data_ref_f32: &mut f32 = unsafe {
-        std::mem::transmute(&mut data[0])
-    };
-    let data_f32 = unsafe {
-        std::slice::from_raw_parts_mut(data_ref_f32, data.len())
-    };
-    data_f32[0] = val.x;
-    data_f32[1] = val.y;
-    data_f32[2] = val.z;
 }
 
 struct Options {
@@ -189,7 +208,7 @@ fn time_func<F, G>(msg: &str, func: F) -> Result<G>
 // Read an input PNG and return its size and contents
 // It must be a certain format (8bpp true color no alpha)
 fn read_png(filename: &str)
-    -> Result<PixelBuffer>
+    -> Result<PixelSource>
 {
     use png::Decoder;
     use png::Transformations;
@@ -206,22 +225,19 @@ fn read_png(filename: &str)
         return Err(PNGFormatError);
     }
 
-    let mut data = vec![0u8; info.buffer_size()];
-    reader.next_frame(&mut data)?;
+    let bytes_per_pixel = 3;
+    let mut buffer = PixelBuffer::new(
+        info.width as usize,
+        info.height as usize,
+        bytes_per_pixel
+    );
+    reader.next_frame(buffer.get_bytes_mut())?;
 
-    Ok(PixelBuffer {
-        bytes_per_pixel: 3,
-        width: info.width as usize,
-        height: info.height as usize,
-        stride: info.width as usize * 3,
-        data: data,
-        read_rgb_func: read_bt2100_rgb24,
-        write_rgb_func: write_srgb_rgb24,
-    })
+    Ok(PixelSource::bt2100_rgb24(buffer))
 }
 
 fn read_jxr(filename: &str)
-  -> Result<PixelBuffer>
+  -> Result<PixelSource>
 {
     use jpegxr::ImageDecode;
     use jpegxr::PixelFormat;
@@ -235,25 +251,19 @@ fn read_jxr(filename: &str)
         return Err(UnsupportedPixelFormat);
     }
 
-    let bytes_per_pixel = 16;
     let (width, height) = decoder.get_size()?;
-    let stride = width as usize * bytes_per_pixel;
-    let size = stride * height as usize;
-    let mut data = Vec::<u8>::with_capacity(size);
-    data.resize(size, 0);
+    let bytes_per_pixel = 16;
+    let stride = width * bytes_per_pixel;
+    let mut buffer = PixelBuffer::new(
+        width as usize,
+        height as usize,
+        bytes_per_pixel as usize
+    );
 
     let rect = Rect::new(0, 0, width, height);
-    decoder.copy(&rect, &mut data, stride as u32)?;
+    decoder.copy(&rect, buffer.get_bytes_mut(), stride as u32)?;
 
-    Ok(PixelBuffer {
-        bytes_per_pixel: bytes_per_pixel,
-        width: width as usize,
-        height: height as usize,
-        stride: stride,
-        data: data,
-        read_rgb_func: read_scrgb_rgb128float,
-        write_rgb_func: write_scrgb_rgb128float,
-    })
+    Ok(PixelSource::scrgb_rgb128float(buffer))
 }
 
 fn pq_to_linear(val: Vec3) -> Vec3 {
@@ -385,7 +395,7 @@ fn hdr_to_sdr_pixel(rgb_scrgb: Vec3, options: &Options) -> Vec3
     val
 }
 
-fn hdr_to_sdr(in_data: &PixelBuffer, out_data: &mut PixelBuffer, options: &Options)
+fn hdr_to_sdr(in_data: &PixelSource, out_data: &mut PixelSink, options: &Options)
 {
     in_data.map_into(out_data, |rgb| hdr_to_sdr_pixel(rgb, options));
 }
@@ -417,7 +427,7 @@ fn write_png(filename: &str, data: &PixelBuffer)
 
 fn hdrfix(args: ArgMatches) -> Result<String> {
     let input_filename = args.value_of("input").unwrap();
-    let in_data = time_func("read_input", || {
+    let source = time_func("read_input", || {
         let ext = Path::new(&input_filename).extension().unwrap().to_str().unwrap();
         match ext {
             "png" => read_png(input_filename),
@@ -443,14 +453,19 @@ fn hdrfix(args: ArgMatches) -> Result<String> {
             _ => unreachable!("bad color-map option")
         },
     };
-    let mut out_data = PixelBuffer::new_srgb_rgb8(in_data.width, in_data.height);
+
+    let width = source.buffer.width as usize;
+    let height = source.buffer.height as usize;
+    let bytes_per_pixel = 3;
+    let buffer = PixelBuffer::new(width, height, bytes_per_pixel);
+    let mut sink = PixelSink::srgb_rgb24(buffer);
     time_func("hdr_to_sdr", || {
-        Ok(hdr_to_sdr(&in_data, &mut out_data, &options))
+        Ok(hdr_to_sdr(&source, &mut sink, &options))
     })?;
 
     let output_filename = args.value_of("output").unwrap();
     time_func("write_png", || {
-        write_png(output_filename, &out_data)
+        write_png(output_filename, &sink.buffer)
     })?;
 
     return Ok(output_filename.to_string());
