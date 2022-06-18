4e597db506a4991ab3783f2424e8d48f584f3919
Brion Vibber <brion@pobox.com>
2022-06-18 14:18:18 -0700

    fix watch.bat

--- watch.bat
+++ watch.bat
@@ -1,9 +1,5 @@
 @rem Watches for new .jxr files and converts them as they appear.
 
 hdrfix ^
-    --pre-gamma=2 ^
-    --exposure=-4 ^
-    --saturation=1.5 ^
-    --post-gamma=0.5 ^
-    --color-map=clip ^
+    --exposure=-1 ^
     --watch=.
